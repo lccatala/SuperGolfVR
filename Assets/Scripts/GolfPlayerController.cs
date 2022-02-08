9bf1c56ed17572a43d816ab2b089230f94fdd589
Luis Carlos Catalá Martínez <lccm1@alu.ua.es>
2022-02-08 20:44:04 +0100

    Ball movement

--- Assets/Scripts/GolfPlayerController.cs
+++ Assets/Scripts/GolfPlayerController.cs
@@ -10,32 +10,64 @@ public class GolfPlayerController : MonoBehaviour
     [SerializeField]
     private float minSpeedToTeleport = 0.5f;
 
+    [SerializeField]
+    private float distanceToBall = 0.3f;
+
     private bool canTeleport = false;
+    private Vector3[] teleportOffsets;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        teleportOffsets = new [] {
+            new Vector3(distanceToBall, 0.0f, 0.0f),
+            new Vector3(-distanceToBall, 0.0f, 0.0f),
+            new Vector3(0.0f, 0.0f, distanceToBall),
+            new Vector3(0.0f, 0.0f, -distanceToBall),
+        };
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (ball.GetComponent<Rigidbody>().velocity.magnitude < minSpeedToTeleport)
+        if (ball.GetComponent<Rigidbody>().velocity.magnitude > minSpeedToTeleport)
+        {
+            canTeleport = true;
+            ball.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else if (canTeleport)
         {
             TeleportNearBall();
             canTeleport = false;
             ball.GetComponent<Renderer>().material.color = Color.green;
         }
-        else
+
+    }
+
+    private Vector3 GetFreePositionOffset()
+    {
+        foreach (Vector3 offset in teleportOffsets)
         {
-            canTeleport = true;
-            ball.GetComponent<Renderer>().material.color = Color.red;
+            // TODO: this doesn't work
+            if (!Physics.CheckBox(ball.transform.position + offset, new Vector3(0.1f, 0.1f, 0.1f), transform.rotation))
+            {
+                return offset;
+            }
         }
+
+        return teleportOffsets[0];
     }
 
     private void TeleportNearBall()
     {
+        Vector3 offset = GetFreePositionOffset();
+        var player = GetComponentInChildren<OVRPlayerController>();
 
+        player.enabled = false;
+        transform.position = new Vector3(
+            ball.transform.position.x + offset.x,
+            transform.position.y,
+            ball.transform.position.z + offset.z);
+        player.enabled = true;
     }
 }
